ee1cf19fffeb0dddaf2aec20217a26b8378ebf60
Punnoose <punnoosekuttyj@yahoo.com>
2014-10-02 01:05:23 -0400

    cosmetic change

--- SpringRestDemo/src/test/java/org/punnoose/spring/rest/web/controller/PersonControllerTest.java
+++ SpringRestDemo/src/test/java/org/punnoose/spring/rest/web/controller/PersonControllerTest.java
@@ -1,9 +1,13 @@
 package org.punnoose.spring.rest.web.controller;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import java.nio.charset.Charset;
@@ -19,7 +23,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.hamcrest.Matchers.*;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
