e4cc607cd3e85179e6c5c5b99a97f8ffbec2b877
Punnoose <punnoosekuttyj@yahoo.com>
2014-10-03 01:10:53 -0400

    test change

--- SpringRestDemo/src/main/java/org/punnoose/spring/rest/domain/Person.java
+++ SpringRestDemo/src/main/java/org/punnoose/spring/rest/domain/Person.java
@@ -3,7 +3,7 @@ package org.punnoose.spring.rest.domain;
 public class Person {
 
 	private String name;
-	private String country="US";
+	private String country="UK";
 	
 	
 	public Person(String name){
